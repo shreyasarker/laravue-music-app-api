d139df62a81e2a3c6213cf36ed4480472f33ef65
Shreya Sarker <shreya.sarker@gmail.com>
2023-04-21 23:24:52 +0600

    Removed guard from Auth in Logout function

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -47,7 +47,7 @@ class AuthController extends Controller
     public function logout(Request $request)
     {
         try {
-            Auth::guard('web')->logout();
+            Auth::logout();
             $request->session()->invalidate();
             $request->session()->regenerateToken();
 
