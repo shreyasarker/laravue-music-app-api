3d565b60b853607fdc887a34565e42b4a985e450
Shreya Sarker <shreya.sarker@gmail.com>
2023-04-23 02:01:35 +0600

    Modified Image to Long Text to be Saved as Base64

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->text('location')->nullable();
             $table->text('description')->nullable();
-            $table->text('image')->nullable();
+            $table->longText('image')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
