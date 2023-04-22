7458898f612ce260b2550d9d44dc1dafd2a6d043
Shreya Sarker <shreya.sarker@gmail.com>
2023-04-22 23:32:18 +0600

    Added Validation for Base64 Image

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Validator::extend('image64', function ($attribute, $value, $parameters, $validator) {
+            $type = explode('/', explode(':', substr($value, 0, strpos($value, ';')))[1])[1];
+            if (in_array($type, $parameters)) {
+                return true;
+            }
+            return false;
+        });
+
+        Validator::replacer('image64', function($message, $attribute, $rule, $parameters) {
+            return str_replace(':values',join(",",$parameters),$message);
+        });
     }
 }
